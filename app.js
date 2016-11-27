f3f6706f5a7909ac189bdd52fb4c8e92455de88e
Shrikshel <devshrikshel@gmail.com>
2016-11-27 11:31:03 +0530

    Middleware Setup

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+
+const expressValidator = require('express-validator');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 var bodyParser = require('body-parser');
+const multer = require('multer');
+const flash = require('connect-flash');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+var db = mongoose.connection;
+
+//Handle File Uploads
+var upload = multer({
+  dest: './uploads'
+})
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,13 +29,54 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+//Handle Express Sessions
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Passports
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Flash
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(res, req);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
